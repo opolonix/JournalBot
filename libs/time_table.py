dbbbd0a1a9911167582a5d5f1c9fab846ab48d0a
OPOLO <misha90638@gmail.com>
2023-09-02 21:06:19 +0300

    Update time_table.py

--- libs/time_table.py
+++ libs/time_table.py
@@ -1,6 +1,8 @@
 import datetime
 from app import week, query
 
+def this_day(text):
+    return datetime.datetime.now().strftime('%d.%m.%Y') == text
 
 def time_table(date):
     
@@ -12,10 +14,12 @@ def time_table(date):
     if even: result += " 🔵"
     else: result += " 🔴"
     day = query(f"SELECT * FROM `{week[date.weekday()][0]}` ORDER BY `{week[date.weekday()][0]}`.`pos` ASC")
+    added = 0
     for i in day:
         if eval(i['if']):
             time_start = datetime.datetime.fromtimestamp(i['subj_start'])
             time_end = datetime.datetime.fromtimestamp(i['subj_end'])
             result += f"\n*{i['subj_name']}*\n  _{i['cabinet']}_ | _{time_start.strftime('%H:%M')} - {time_end.strftime('%H:%M')}_"
+            added += 1
 
     return result
