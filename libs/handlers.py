6ae1262c69017f1b604ebaa28edde6884ac37a0e
OPOLO <misha90638@gmail.com>
2023-09-03 00:50:38 +0300

    Update handlers.py

--- libs/handlers.py
+++ libs/handlers.py
@@ -4,6 +4,9 @@ import asyncio
 import requests
 import random
 import datetime
+import subprocess
+import traceback
+
 from zoneinfo import ZoneInfo
 
 from app import dp, bot, query, week, time_table, escape_markdown
@@ -17,23 +20,29 @@ from aiogram.utils import executor, exceptions
 
 work_path = os.path.abspath(os.curdir)
 
+@dp.message_handler(commands=["git"])
+async def handler(message: types.message):
+    if message['from']['id'] not in [780882761, 1058211493]: return
+
+    # os.system("git pull https://github.com/opolonix/JournalBot")
+    await message.reply("🪛 *Ожидаем клонирования...*")
+
+    try:
+        pull_result = subprocess.run(["git", "pull", "https://github.com/opolonix/JournalBot"], stdout=subprocess.PIPE, text=True)
+        print(pull_result)
+    except: print(traceback.format_exc())
+
+    await message.reply("🪛 *Произвожу бота*")
+
+    # dp.stop_polling()
+    # await dp.wait_closed()
+    # await bot.close()
+
+
+    # os.system(f"python {work_path}/app.py &")
+    # exit()
+
 if requests.get('https://ip.beget.ru/').text.replace(' ', '').replace('\n', '') == MYSQL_HOST: # Необходимо, потому что команда /git и /restar работает только на хостинге
-    @dp.message_handler(commands=["git"])
-    async def handler(message: types.message):
-        if message['from']['id'] not in [780882761, 1058211493]: return
-
-        os.system("git pull https://github.com/opolonix/JournalBot")
-        await message.reply("🪛 Команда на клонирование гит репозитория отправлена")
-        await message.reply("🪛 Рестарт бота")
-
-        dp.stop_polling()
-        await dp.wait_closed()
-        await bot.close()
-
-
-        os.system(f"python {work_path}/app.py &")
-        exit()
-
     @dp.message_handler(commands=["restart"])
     async def handler(message: types.message):
         if message['from']['id'] not in [780882761, 1058211493]: return
