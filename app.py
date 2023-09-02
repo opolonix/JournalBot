fcfd9fa5059bfb371a00aa1335a9b51b3b2ca1cb
OPOLO <misha90638@gmail.com>
2023-09-02 21:06:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 import sys
 import asyncio
 import time
+import re
 
 from config import BOT_TOKEN, OWNER_ID
 
@@ -17,17 +18,23 @@ from libs.time_table import time_table
 
 sys.path.append(os.path.abspath(os.curdir) + "/libs")
 
+def escape_markdown(text):
+    """функция для экранирования символов перед отправкой в маркдауне телеграма"""
+    pattern = r"([_*\[\]~|`])"
+    return re.sub(pattern, r"\\\1", text)
+
 bot = Bot(
     token = BOT_TOKEN
 )
 dp = Dispatcher(bot)
 
 async def on_startup(dp):
-    await dp.bot.send_message(OWNER_ID, "Бот запущен")
+    await dp.bot.send_message(OWNER_ID, f"*Вход!* _(⏰{datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')})_", parse_mode="Markdown")
 
 if __name__ == '__main__':
     from libs.handlers import dp
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+
 
 
 
