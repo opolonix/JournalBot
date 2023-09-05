2fff0c542339b1eff2d3d0d3d351f397ef74be8e
OPOLO <misha90638@gmail.com>
2023-09-05 22:04:25 +0300

    Update days_data.py

--- libs/days_data.py
+++ libs/days_data.py
@@ -1,3 +1,5 @@
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
 week = [
     ["monday", "Понедельник"],
     ["tuesday", "Вторник"],
@@ -7,3 +9,12 @@ week = [
     ["saturday", "Суббота"],
     ["sunday", "Воскресенье"]
 ]
+# timetable = KeyboardButton('Расписание')
+# teachers = KeyboardButton('Учителя')
+# group = KeyboardButton('Группа')
+# timetableBtn = ReplyKeyboardMarkup(resize_keyboard=True)
+# timetableBtn.add(timetable).add(teachers, group)
+# position = ReplyKeyboardMarkup().row(timetable, teachers, group)
+
+timetable = KeyboardButton('Расписание')
+timetableBtn = ReplyKeyboardMarkup(resize_keyboard=True).row(timetable)
